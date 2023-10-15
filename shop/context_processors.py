74b12a23500b409801db5a5ce39306af0e8ab9fd
Becky Smith <rebkwok@gmail.com>
2023-10-15 15:56:03 +0100

    hide search option

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,6 +1,9 @@
+from os import environ
+
 from .views import get_basket_quantity
 
 def shop_context(request):
     return {
-        "basket_quantity": get_basket_quantity(request)
+        "basket_quantity": get_basket_quantity(request),
+        "hide_search": environ.get("HIDE_SEARCH", False)
     }
