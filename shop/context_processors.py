68f81d58a60c60d1cb3eec05c2342a96052b46f7
Becky Smith <rebkwok@gmail.com>
2024-01-07 13:44:17 +0000

    Add sales banner

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,9 +1,11 @@
 from os import environ
 
+from .models import Sale
 from .views import get_basket_quantity
 
 def shop_context(request):
     return {
         "basket_quantity": get_basket_quantity(request),
-        "hide_search": environ.get("HIDE_SEARCH", False)
+        "hide_search": environ.get("HIDE_SEARCH", False),
+        "current_sale": Sale.current_sale(),
     }
